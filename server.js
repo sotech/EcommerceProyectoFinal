bd4a29433ba6caed645086d367bfca6f4756ff78
German Sommariva <germansommariva@gmail.com>
2021-05-06 09:25:31 -0300

    Establecido Mongo como persistencia unica

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const app = express();
-const rutasProductos_fs = require('./rutas/rutasProductos_fs');
-const rutasProductos_sqlite = require('./rutas/rutasProductos_sqlite');
-const rutasProductos_mariadb = require('./rutas/rutasProductos_mariadb');
+//const rutasProductos_fs = require('./rutas/rutasProductos_fs');
+//const rutasProductos_sqlite = require('./rutas/rutasProductos_sqlite');
+//const rutasProductos_mariadb = require('./rutas/rutasProductos_mariadb');
 const rutasProductos_mongoLocal = require('./rutas/rutasProductos_mongoLocal');
-const rutasCarrito_fs = require('./rutas/rutasCarrito_fs');
-const rutasCarrito_sqlite = require('./rutas/rutasCarrito_sqlite');
-const rutasCarrito_mariadb = require('./rutas/rutasCarrito_mariadb');
-const rutasCarrito_mongoLocal = require('./rutas/rutasCarrito_mongoLocal');
+//const rutasCarrito_fs = require('./rutas/rutasCarrito_fs');
+//const rutasCarrito_sqlite = require('./rutas/rutasCarrito_sqlite');
+//const rutasCarrito_mariadb = require('./rutas/rutasCarrito_mariadb');
+//const rutasCarrito_mongoLocal = require('./rutas/rutasCarrito_mongoLocal');
 
 const port = process.env.PORT || 8080;
 
@@ -20,9 +20,9 @@ app.use(express.urlencoded({ extended: true }));
  * 3: MariaDB
  * 4: Mongo
  */
-const persistenciaElegida = 3;
+const persistenciaElegida = 4;
 
-switch(persistenciaElegida){
+/*switch(persistenciaElegida){
     case 1:
         app.use('/api', rutasProductos_fs);
         app.use('/api', rutasCarrito_fs);
@@ -40,6 +40,8 @@ switch(persistenciaElegida){
         app.use('/api', rutasCarrito_mongoLocal);
         break;
 }
+*/
+app.use('/api', rutasProductos_mongoLocal);
 
 app.get('/', (req, res) => {
     res.send("Servidor iniciado");
