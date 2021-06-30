fc10044671bc026f61ebb37f3dac80faaaa53dab
German Sommariva <germansommariva@gmail.com>
2021-06-30 12:28:33 -0300

    Conexion a MongoAtlas

---
+++ src/utils/mongoConnection.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+
+db.on('error', () => {
+  console.log('Error al conectarse a MongoAtlas')
+});
+
+db.once('open', () => {
+  console.log('Conectado a MongoAtlas')
+});
+
+module.exports = db;
