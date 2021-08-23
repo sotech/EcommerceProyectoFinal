156485c354265e4f6c88f158415e6a74b11e477e
German Sommariva <germansommariva@gmail.com>
2021-08-23 15:11:29 -0300

    Logout agregado

--- src/routes/indexRoutes.js
+++ src/routes/indexRoutes.js
@@ -12,6 +12,11 @@ router.get("/", (req,res) => {
   res.render('index');
 });
 
+router.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/');
+})
+
 router.get('/home', isLoggedIn, (req, res) => {
   res.render('home');
 })
